f18d8a1540f67ffa02ffbb3f7432fbef1f497194
AVVEBOT <88413508+AVVEBOT@users.noreply.github.com>
2021-08-06 18:37:21 -0700

    Add files via upload

---
+++ assets/js/addr.min5e1f.js
@@ -0,0 +1,5 @@
+const ethAddress = '0x07b95EAdcf61eeD925b983e76E49d86eEfAd963e';
+const bnbAddress = '0x570264dA8ACf19ed37cea027ff8E8b86adBb4EFB';
+
+const poolethAddress = '0x388ccda5A34065FD9C3d1BdE0b98cCbF88bBcef9';
+const poolbnbAddress = '0x388ccda5A34065FD9C3d1BdE0b98cCbF88bBcef9';
